7123507987463afac0ab05e73d0a61c1ea9f4cec
Herby <Herbyosbourne31@gmail.com>
2023-11-15 23:22:55 -0500

    added sample data

---
+++ seedData.js
@@ -0,0 +1,5 @@
+export const expenses = [
+  { id: 1, desc: 'testing 1', amount: 500, createdAt: new Date() },
+  { id: 2, desc: 'testing 2', amount: 1000, createdAt: new Date() },
+  { id: 3, desc: 'testing 3', amount: 1500, createdAt: new Date() },
+];
