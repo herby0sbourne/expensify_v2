c93022993243288711192695fd361e944c7858dd
Herby <Herbyosbourne31@gmail.com>
2023-11-16 00:04:57 -0500

    create new route to edit expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/header/Header.jsx";
-import AddExpensePage from "./pages/addExpensePage/AddExpensePage.jsx";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header.jsx';
+import AddExpensePage from './pages/addExpensePage/AddExpensePage.jsx';
+import EditExpensePage from './pages/editExpensePage/EditExpensePage.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Header />} />
       <Route path="/create" element={<AddExpensePage />} />
+      <Route path="/edit/:id" element={<EditExpensePage />} />
     </Routes>
   );
 };
