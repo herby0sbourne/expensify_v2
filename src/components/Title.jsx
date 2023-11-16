2be64dd3ef991da61b38e6794feca01cd6597033
Herby <Herbyosbourne31@gmail.com>
2023-11-15 23:14:55 -0500

    create title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+const Title = ({ heading }) => {
+  return (
+    <div className="bg-off-white py-l-size px-[0] mb-l-size">
+      <div className="container">
+        <h1 className="font-light">{heading}</h1>
+      </div>
+    </div>
+  );
+};
+export default Title;
