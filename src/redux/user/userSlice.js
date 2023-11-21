25d9906b362bafe5ef319a7b85b9ca69c4fb4986
Herby <Herbyosbourne31@gmail.com>
2023-11-21 09:26:57 -0500

    remove unnecessary comment

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,15 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {},
   reducers: {
     login: (state, action) => {
-      state.uid = action.payload?.uid;
+      state.uid = action.payload?.uid || null;
     },
   },
 });
-// uid:'hH9PD9EcKkhUi3Ae8Evxk9XRV6F3'
+
 const userReducer = userSlice.reducer;
 
 export const { login } = userSlice.actions;
