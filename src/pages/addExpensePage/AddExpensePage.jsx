48eab1682cba00607917241979377e7231555768
Herby <Herbyosbourne31@gmail.com>
2023-11-21 09:44:24 -0500

    added function to add expense to database

--- src/pages/addExpensePage/AddExpensePage.jsx
+++ src/pages/addExpensePage/AddExpensePage.jsx
@@ -1,15 +1,24 @@
-import Title from '../../components/Title.jsx';
-import ExpenseForm from '../../components/expenseForm/ExpenseForm.jsx';
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import Title from "../../components/Title.jsx";
+import ExpenseForm from "../../components/expenseForm/ExpenseForm.jsx";
+
+import { startAddExpense } from "../../redux/expense/expenseSlice.js";
 
 const AddExpensePage = () => {
-  const submitExpense = (expense) => {
-    console.log(expense);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const addExpense = (expense) => {
+    dispatch(startAddExpense(expense));
+    navigate("/");
   };
   return (
     <div>
       <Title heading="Add Expenses" />
       <div className="container">
-        <ExpenseForm onSubmit={submitExpense} />
+        <ExpenseForm onSubmit={addExpense} />
       </div>
     </div>
   );
