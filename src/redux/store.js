d0a2f234f3ce2d093bc8f2c645611fb770895ff7
Herby <Herbyosbourne31@gmail.com>
2023-11-21 09:41:09 -0500

    added expenses reducer and remove console log

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import userReducer from './user/userSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import userReducer from "./user/userSlice";
+import expenseReducer from "./expense/expenseSlice.js";
 
 const middlewares = [];
 
-if (import.meta.env.VITE_NODE_ENV === 'development') {
-  console.log(import.meta.env.VITE_NODE_ENV);
+if (import.meta.env.VITE_NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    expenses: expenseReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(middlewares),
-  devTools: import.meta.env.VITE_NODE_ENV !== 'production',
+  devTools: import.meta.env.VITE_NODE_ENV !== "production",
 });
 export default store;
