31c34a9528550e26890099601148e5f7164fa594
Herby <Herbyosbourne31@gmail.com>
2023-11-25 10:36:29 -0500

    uncomment expensesListFilter

--- src/pages/expenseDashboardPage/ExpenseDashboardPage.jsx
+++ src/pages/expenseDashboardPage/ExpenseDashboardPage.jsx
@@ -1,11 +1,12 @@
 import ExpenseList from "../../components/expenseList/ExpenseList.jsx";
 import ExpensesSummary from "../../components/expensesSummary/ExpensesSummary.jsx";
+import ExpenseListFilters from "../../components/expenseListFilters/expenseListFilters.jsx";
 
 const ExpenseDashboardPage = () => {
   return (
     <>
       <ExpensesSummary />
-      {/*<ExpenseListFilters />*/}
+      <ExpenseListFilters />
       <ExpenseList />
     </>
   );
