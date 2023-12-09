f0498768ca3275a8304d22a0abd5cb926c460553
Herby <Herbyosbourne31@gmail.com>
2023-12-09 15:04:17 -0500

    create and import loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import LoginPage from "./pages/loginPage/LoginPage.jsx";
@@ -8,20 +8,28 @@ import EditExpensePage from "./pages/editExpensePage/EditExpensePage.jsx";
 import ExpenseDashboardPage from "./pages/expenseDashboardPage/ExpenseDashboardPage";
 
 import Layout from "./components/Layout.jsx";
+import Loader from "./components/loader.jsx";
 import PublicRoute from "./components/protectedRoute/PublicRoute.jsx";
 import ProtectedRoute from "./components/protectedRoute/ProtectedRoute.jsx";
 
-import "./App.css";
 import { ListeningToAuthChanges } from "./redux/auth.js";
+import { selectLoading } from "./redux/loading/loadingSlice.js";
+
+import "./App.css";
 
 const App = () => {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
 
   useEffect(() => {
     const unsubscribe = dispatch(ListeningToAuthChanges());
 
     return () => unsubscribe();
   }, [dispatch]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <Routes>
