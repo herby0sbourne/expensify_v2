c142890c098227cdb79872e920d611d1f7c4009f
Herby <Herbyosbourne31@gmail.com>
2023-12-09 15:07:30 -0500

    call dispatch to show and remove loader

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -1,14 +1,33 @@
-import { onAuthStateChangedListener } from "../firebase/firebase";
-import { startGetExpenses } from "./expense/expenseSlice";
 import { login } from "./user/userSlice";
+import { startGetExpenses } from "./expense/expenseSlice";
+import { onAuthStateChangedListener } from "../firebase/firebase";
+import { clearLoading, setLoading } from "./loading/loadingSlice";
+
+// export const ListeningToAuthChanges = () => (dispatch) => {
+//   dispatch(setLoading());
+//   return onAuthStateChangedListener((user) => {
+//     dispatch(clearLoading());
+//     if (user) {
+//       dispatch(login(user));
+//       dispatch(startGetExpenses());
+//     } else {
+//       dispatch(login(user));
+//     }
+//   });
+// };
 
 export const ListeningToAuthChanges = () => (dispatch) => {
+  dispatch(setLoading());
   return onAuthStateChangedListener((user) => {
-    if (user) {
+    if (!user) {
       dispatch(login(user));
-      dispatch(startGetExpenses());
-    } else {
-      dispatch(login(user));
+      dispatch(clearLoading());
+      return;
     }
+
+    dispatch(login(user));
+    dispatch(startGetExpenses());
+
+    dispatch(clearLoading());
   });
 };
